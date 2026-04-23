209ca4c6b062f06410f1614bfb8ecb162febe528
Diego <diego.fobs@gmail.com>
2026-04-22 21:00:12 -0300

    Update Jokenpo.py

--- Jokenpo.py
+++ Jokenpo.py
@@ -7,12 +7,12 @@ import winsound
 
 musica = random.randint(1, 3)
 
-# if musica == 1:
-#     winsound.PlaySound("sounds/music.wav", winsound.SND_FILENAME | winsound.SND_ASYNC | winsound.SND_LOOP)
-# elif musica == 2:
-#     winsound.PlaySound("sounds/music2.wav", winsound.SND_FILENAME | winsound.SND_ASYNC | winsound.SND_LOOP)
-# else:
-#     winsound.PlaySound("sounds/music3.wav", winsound.SND_FILENAME | winsound.SND_ASYNC | winsound.SND_LOOP)
+if musica == 1:
+    winsound.PlaySound("sounds/music.wav", winsound.SND_FILENAME | winsound.SND_ASYNC | winsound.SND_LOOP)
+elif musica == 2:
+    winsound.PlaySound("sounds/music2.wav", winsound.SND_FILENAME | winsound.SND_ASYNC | winsound.SND_LOOP)
+else:
+    winsound.PlaySound("sounds/music3.wav", winsound.SND_FILENAME | winsound.SND_ASYNC | winsound.SND_LOOP)
 
 j1 = 0
 nome1 = ""
